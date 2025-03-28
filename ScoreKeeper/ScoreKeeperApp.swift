8b1f78a16ef3ff21f0cf0a9cb5bfb74e3b879657
bmmuceo <bmmuceo@gmail.com>
2025-03-28 07:30:42 -0400

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by RyanA on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
